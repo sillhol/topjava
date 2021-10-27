b3e780a21f4a998b8e2baf4e44821142e3168ee4
sillhol <sillhol@gmail.com>
2021-10-27 22:09:09 +0300

    1_0_fix patch

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -2,7 +2,7 @@ package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
 
-public class UserMealWithExceed {
+public class MealTo {
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -11,7 +11,7 @@ public class UserMealWithExceed {
 
     private final boolean excess;
 
-    public UserMealWithExceed(LocalDateTime dateTime, String description, int calories, boolean excess) {
+    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
